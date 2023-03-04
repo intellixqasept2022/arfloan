5ab2234af270ae72aa5fc1758ddb20f6bcc343eb
intel <intel@DESKTOP-AQ79MK3.mshome.net>
2023-03-03 19:26:53 -0500

    Updated

--- Test Suites/LoginTestSuite.ts
+++ Test Suites/LoginTestSuite.ts
@@ -11,18 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>8dffc068-b711-43e3-ab1e-784329940a1b</testSuiteGuid>
-   <testCaseLink>
-      <guid>e4b32d75-8f3d-454f-98bb-9ec4db9c1ee3</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/DealerUserLogin</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>706e449b-1f4a-423f-8910-d999fdcd9ede</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/SuperUserLogin</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
 </TestSuiteEntity>
